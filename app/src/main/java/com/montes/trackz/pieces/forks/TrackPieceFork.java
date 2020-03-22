f0a7913e16a548b9be9dd477c567e3088d75f30f
Szymon Filipiak <szymonfilipiak98@gmail.com>
2020-03-22 19:51:07 +0100

    Add abstract TrackPieceFork class. Don't know yet, how to develop it.

---
+++ app/src/main/java/com/montes/trackz/pieces/forks/TrackPieceFork.java
@@ -0,0 +1,10 @@
+package com.montes.trackz.pieces.forks;
+
+import com.montes.trackz.pieces.TrackPiece;
+
+public abstract class TrackPieceFork extends TrackPiece {
+
+    public TrackPieceFork(String id, String name, double length) {
+        super(id, name, length);
+    }
+}
